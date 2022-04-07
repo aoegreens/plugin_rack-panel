130a14a8473b3f909a991b67ad6f14f24dc581a4
eons <eons@eons.dev>
2022-04-06 18:44:01 -0700

    Everyone's favorite
    Fixes bugs and adds  features!

---
+++ inc/mods.php
@@ -0,0 +1,3 @@
+<?php namespace aoe;
+
+add_filter( 'fl_enable_shortcode_css_js', '__return_true' );
--- inc/shortcode.php
+++ inc/shortcode.php
@@ -2,23 +2,58 @@
 
 /**
  * @param device the name of the device to query "e.g. a0201"
- * @param pin the pin number to query; for raspberry pi, this is the wiringPi number.
+ * @param gpio the pin number to query; for raspberry pi, this is the wiringPi number.
  * @return the value of the given gpio on the given device.
  */
-function aoe_read_gpio($device, $gpio) {
-
-    $url = "http://forwarder-1-svc:80/v1/gpio/get";
-    
-    $data = array();
-    $data["device"] = $device;
-    $data["pin"] = $gpio;
-
-    $url = sprintf("%s?%s", $url, http_build_query($data));
-    
-    $curl = curl_init();
+function aoe_read_gpio($args) {
+    $url = "http://forwarder-1-svc:80/v1/gpio/get?device=".$args["device"]."&pin=".$args["gpio"];
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($curl);
     curl_close($curl);
-
-    return $result
+    return $result;
 }
-add_shortcode('aoe_read_gpio',  __NAMESPACE__ . '\\' . 'pod_field_from_user_profile');
+add_shortcode('aoe_read_gpio',  __NAMESPACE__ . '\\' . 'aoe_read_gpio');
+
+function aoe_current_device_status() {
+    $device = pods('device', get_the_ID());
+    $read_arr = array();
+    $read_arr["device"] = $device->field("controller_name");
+    $read_arr["gpio"] = $device->field("gpio_pin");
+    return aoe_read_gpio($read_arr);
+}
+add_shortcode('aoe_current_device_status',  __NAMESPACE__ . '\\' . 'aoe_current_device_status');
+
+/**
+ * @param device the name of the device to query "e.g. a0201"
+ * @param gpio the pin number to query; for raspberry pi, this is the wiringPi number.
+ * @return NULL.
+ */
+function aoe_toggle_gpio($args) {
+    $url = "http://forwarder-1-svc:80/v1/gpio/set";
+    $payload = json_encode(array(
+        "device" => $args["device"],
+        "pin" => $args["gpio"],
+        "state" => "TOGGLE"
+    ));
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    //$result is unused.
+    return;
+}
+add_shortcode('aoe_toggle_gpio',  __NAMESPACE__ . '\\' . 'aoe_toggle_gpio');
+
+function aoe_toggle_current_device() {
+    $device = pods('device', get_the_ID());
+    $write_arr = array();
+    $write_arr["device"] = $device->field("controller_name");
+    $write_arr["gpio"] = $device->field("gpio_pin");
+    aoe_toggle_gpio($write_arr);
+    header("refresh:5;url=https://aoegreens.com/garden-dashboard/");
+}
+add_shortcode('aoe_toggle_current_device',  __NAMESPACE__ . '\\' . 'aoe_toggle_current_device');
